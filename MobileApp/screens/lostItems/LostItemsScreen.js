ab59b90878e3661b7aa47abd2627729f3996ccb5
sandaliranasinghe <sandyblaq99@gmail.com>
2022-08-19 12:47:13 +0530

    Lost-items changes

--- MobileApp/screens/lostItems/LostItemsScreen.js
+++ MobileApp/screens/lostItems/LostItemsScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -25,7 +25,10 @@ const LostItemsScreen = () => {
         />
       </View>
       <View style={[styles.mainBody, tw`mt-2`]}>
-        <View style={tw`flex-row h-40 bg-[#e4e7e7] rounded-xl p-4 mt-5`}>
+        {/* <TouchableOpacity> */}
+        <TouchableOpacity
+          style={tw`flex-row h-40 bg-[#e4e7e7] rounded-xl p-4 mt-5`}
+        >
           <Image
             style={tw`h-25 w-25 flex-0.25 `}
             // blurRadius={10}
@@ -54,67 +57,8 @@ const LostItemsScreen = () => {
               <FontAwesomeIcon icon={faClockRotateLeft} color={"#444444"} />
             </View>
           </View>
-        </View>
-        {/* <View style={tw`flex-row h-40 bg-[#dae8dd] rounded-xl p-4 mt-5`}>
-          <Image
-            style={tw`h-25 w-25 flex-0.25 `}
-            // blurRadius={10}
-            // resizeMode="cover"
-            source={{
-              uri: "https://3dicons.sgp1.cdn.digitaloceanspaces.com/v1/dynamic/premium/wallet-dynamic-premium.png",
-            }}
-            resizeMode={"cover"}
-          />
-          <View style={[tw`flex-col flex-0.75`]}>
-            <Text
-              style={[
-                tw`px-4 text-base text-justify flex-auto text-[#444444]`,
-                { flexWrap: "wrap" },
-              ]}
-              numberOfLines={4}
-            >
-              On 01.08.2022, a leather wallet was left in the train running from
-              Mirigama to Kolaba Fort. National Identity Card , Bank Cards and
-              Driving License
-            </Text>
-            <View style={[tw`flex-row px-4 flex-row-reverse`]}>
-              <Text style={tw`pl-2 text-[#444444] font-semibold`}>
-                3 hrs ago
-              </Text>
-              <FontAwesomeIcon icon={faClockRotateLeft} color={"#444444"} />
-            </View>
-          </View>
-        </View> */}
-        {/* <View style={tw`flex-row h-40 bg-[#dae8dd] rounded-xl p-4 mt-5`}>
-          <Image
-            style={tw`h-25 w-25 flex-0.25 `}
-            // blurRadius={10}
-            // resizeMode="cover"
-            source={{
-              uri: "https://3dicons.sgp1.cdn.digitaloceanspaces.com/v1/dynamic/premium/wallet-dynamic-premium.png",
-            }}
-            resizeMode={"cover"}
-          />
-          <View style={[tw`flex-col flex-0.75`]}>
-            <Text
-              style={[
-                tw`px-4 text-base text-justify flex-auto text-[#444444]`,
-                { flexWrap: "wrap" },
-              ]}
-              numberOfLines={4}
-            >
-              On 01.08.2022, a leather wallet was left in the train running from
-              Mirigama to Kolaba Fort. National Identity Card , Bank Cards and
-              Driving License
-            </Text>
-            <View style={[tw`flex-row px-4 flex-row-reverse`]}>
-              <Text style={tw`pl-2 text-[#444444] font-semibold`}>
-                3 hrs ago
-              </Text>
-              <FontAwesomeIcon icon={faClockRotateLeft} color={"#444444"} />
-            </View>
-          </View>
-        </View> */}
+        </TouchableOpacity>
+        {/* </TouchableOpacity> */}
       </View>
     </View>
   );
