8820dc0ce8e5374bcbe3e9b126f5a694621df953
kasunjalitha <kasunjalitha77@gmail.com>
2022-08-21 21:37:27 +0530

    jwt user role checking added

--- MobileApp/screens/liveUpdates/TrainLiveUpdatesScreen.js
+++ MobileApp/screens/liveUpdates/TrainLiveUpdatesScreen.js
@@ -1,31 +1,46 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import tw from "twrnc";
 import { useNavigation } from "@react-navigation/native";
 import TopBar from "../../components/navigation/TopBar";
+import { AuthContext } from "../../context/AuthContext";
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
 const TrainLiveUpdatesScreen = () => {
   const navigation = useNavigation();
+
+  const { accessToken } = useContext(AuthContext);
+  const [isModerator, setIsModerator] = useState(false);
+
+  useEffect(() => {
+    var decoded = jwt_decode(accessToken);
+    if (decoded.role == "moderator" || decoded.role == "admin") {
+      setIsModerator(true);
+    }
+  }, []);
 
   return (
     <View>
       <TopBar title="Live Updates" goBack={true} />
       <View style={tw`px-3`}>
-        <TouchableOpacity
-          style={[
-            tw`flex flex-row justify-center items-center bg-[#0074B7] p-2.6 rounded-md`,
-          ]}
-          onPress={() => navigation.navigate("AvailableTrains")}
-        >
-          <MaterialCommunityIcons
-            style={tw`pr-3`}
-            name="train-car-passenger"
-            size={20}
-            color="white"
-          />
-          <Text style={tw`font-medium text-white`}>UPDATE TRAINS</Text>
-        </TouchableOpacity>
+        {isModerator && (
+          <TouchableOpacity
+            style={[
+              tw`flex flex-row justify-center items-center bg-[#0074B7] p-2.6 rounded-md`,
+            ]}
+            onPress={() => navigation.navigate("AvailableTrains")}
+          >
+            <MaterialCommunityIcons
+              style={tw`pr-3`}
+              name="train-car-passenger"
+              size={20}
+              color="white"
+            />
+            <Text style={tw`font-medium text-white`}>UPDATE TRAINS</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
