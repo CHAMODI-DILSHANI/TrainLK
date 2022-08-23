1e464503669d6da6a3df80ca4a0dc1d5b8131d20
Deenath Geeganage <deenathgeeganage@gmail.com>
2022-08-24 00:17:52 +0530

    get post delete implemented for items

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,12 @@ const userRouter = require("./routes/user.router");
 const authRouter = require("./routes/auth.router");
 const stationsRouter = require("./routes/stations.router");
 const scheduleRouter = require("./routes/schedule.router");
+const itemsRouter = require("./routes/items.router");
 
 Router.use("/users", userRouter);
 Router.use("/auth", authRouter);
 Router.use("/stations", stationsRouter);
 Router.use("/schedules", scheduleRouter);
+Router.use("/items", itemsRouter);
 
 module.exports = Router;
