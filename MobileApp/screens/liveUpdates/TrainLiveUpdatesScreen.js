899a190bc1d8123d2a2d25719c3ebf282e23574c
kasunjalitha <kasunjalitha77@gmail.com>
2022-08-23 18:42:01 +0530

    mobile app live update card

--- MobileApp/screens/liveUpdates/TrainLiveUpdatesScreen.js
+++ MobileApp/screens/liveUpdates/TrainLiveUpdatesScreen.js
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React, { useContext } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import tw from "twrnc";
@@ -7,12 +13,87 @@ import TopBar from "../../components/navigation/TopBar";
 import { AuthContext } from "../../context/AuthContext";
 import { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faTrainSubway,
+  faCaretRight,
+  faCaretDown,
+  faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
+import LiveUpdateCard from "../../components/liveUpdates/LiveUpdateCard";
 
 const TrainLiveUpdatesScreen = () => {
   const navigation = useNavigation();
 
   const { accessToken } = useContext(AuthContext);
   const [isModerator, setIsModerator] = useState(false);
+
+  const data1 = {
+    start: "Maradana",
+    end: "Beliattha",
+    status: {
+      stationId: "004",
+      station: "Moratuwa",
+      mode: "stopped",
+      time: "11:45AM",
+    },
+    stations: [
+      { id: "001", name: "colombo fort" },
+      { id: "002", name: "colombo fort" },
+      { id: "003", name: "colombo fort" },
+      { id: "004", name: "colombo fort" },
+      { id: "005", name: "colombo fort" },
+      { id: "006", name: "colombo fort" },
+      { id: "007", name: "colombo fort" },
+    ],
+    lastUpdateTime: "11:12",
+  };
+
+  const data2 = {
+    start: "Maradana",
+    end: "Beliattha",
+    status: {
+      stationId: "004",
+      station: "Moratuwa",
+      mode: "passing",
+      time: "11:45AM",
+    },
+    stations: [
+      { id: "001", name: "colombo fort" },
+      { id: "002", name: "colombo fort" },
+      { id: "003", name: "colombo fort" },
+      { id: "004", name: "colombo fort" },
+      { id: "005", name: "colombo fort" },
+      { id: "006", name: "colombo fort" },
+      { id: "007", name: "colombo fort" },
+      { id: "008", name: "colombo fort" },
+      { id: "009", name: "colombo fort" },
+      { id: "010", name: "colombo fort" },
+      { id: "011", name: "colombo fort" },
+    ],
+    lastUpdateTime: "11:12",
+  };
+
+  const data3 = {
+    start: "Maradana",
+    end: "Beliattha",
+    status: {
+      stationId: "004",
+      station: "Moratuwa",
+      mode: "in",
+      time: "11:45AM",
+    },
+    stations: [
+      { id: "001", name: "colombo fort" },
+      { id: "002", name: "colombo fort" },
+      { id: "003", name: "colombo fort" },
+      { id: "004", name: "colombo fort" },
+      { id: "005", name: "colombo fort" },
+      { id: "006", name: "colombo fort" },
+      { id: "007", name: "colombo fort" },
+    ],
+    lastUpdateTime: "11:12",
+  };
 
   useEffect(() => {
     var decoded = jwt_decode(accessToken);
@@ -22,7 +103,7 @@ const TrainLiveUpdatesScreen = () => {
   }, []);
 
   return (
-    <View>
+    <ScrollView>
       <TopBar title="Live Updates" goBack={true} />
       <View style={tw`px-3`}>
         {isModerator && (
@@ -42,7 +123,15 @@ const TrainLiveUpdatesScreen = () => {
           </TouchableOpacity>
         )}
       </View>
-    </View>
+
+      <View style={tw`p-3`}>
+        {/* CARD */}
+        <LiveUpdateCard data={data1} />
+        <LiveUpdateCard data={data2} />
+        <LiveUpdateCard data={data3} />
+        <LiveUpdateCard data={data1} />
+      </View>
+    </ScrollView>
   );
 };
 
--- MobileApp/screens/profile/ProfileScreen.js
+++ MobileApp/screens/profile/ProfileScreen.js
@@ -1,16 +1,78 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import TopBar from "../../components/navigation/TopBar";
 import tw from "twrnc";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import jwt_decode from "jwt-decode";
 
 const ProfileScreen = () => {
   const { logout } = useContext(AuthContext);
+  const [userInfo, setUserInfo] = useState(null);
+  const { accessToken } = useContext(AuthContext);
+
+  useEffect(() => {
+    var decoded = jwt_decode(accessToken);
+    console.log(decoded);
+
+    function fetchProfile() {
+      const endpoint = `http://192.168.1.100:8080/api/v1/users/profile?id=${decoded.id}`;
+      fetch(endpoint)
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          setUserInfo(json);
+        });
+    }
+
+    fetchProfile();
+  }, []);
 
   return (
     <View style={tw`bg-white`}>
       <TopBar title="Profile" goBack={true} />
+
+      <View style={tw`pt-4 flex-col items-center`}>
+        <Image
+          style={tw`w-20 h-20 rounded-50 border border-gray-400 p-2`}
+          source={{
+            uri: `${userInfo.picture}`,
+          }}
+        />
+
+        <View style={tw`flex-row mt-2 items-center`}>
+          <Text style={tw`text-blue-400 font-black mr-2`}>
+            {userInfo && userInfo.accountType}
+          </Text>
+
+          {userInfo &&
+            userInfo.accountType &&
+            userInfo.accountType == "Moderator" && (
+              <FontAwesomeIcon icon={faCheckCircle} style={tw`text-blue-400`} />
+            )}
+        </View>
+      </View>
+
+      <View style={tw`flex-col mb-4 bg-gray-100 p-3 rounded-2 m-3`}>
+        <View style={tw`flex-row mb-4`}>
+          <Text style={tw`font-bold`}>First Name: </Text>
+          <Text>{userInfo && userInfo.firstName}</Text>
+        </View>
+
+        <View style={tw`flex-row mb-4`}>
+          <Text style={tw`font-bold`}>Last Name: </Text>
+          <Text>{userInfo && userInfo.lastName}</Text>
+        </View>
+
+        {userInfo && userInfo.email && (
+          <View style={tw`flex-row`}>
+            <Text style={tw`font-bold`}>Email: </Text>
+            <Text>{userInfo && userInfo.email ? userInfo.email : ""}</Text>
+          </View>
+        )}
+      </View>
 
       <TouchableOpacity
         onPress={() => {
@@ -18,7 +80,7 @@ const ProfileScreen = () => {
         }}
       >
         <View style={tw`p-2 bg-gray-200 m-3 rounded-2`}>
-          <Text style={tw`text-center text-blue-400 font-medium`}>logout</Text>
+          <Text style={tw`text-center text-blue-400 font-black`}>logout</Text>
         </View>
       </TouchableOpacity>
     </View>
