39823665f0f01198a1f84fb92e707910ca90b12a
Deenath Geeganage <deenathgeeganage@gmail.com>
2022-10-26 06:24:41 +0530

    logging app change state

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,8 +1,42 @@
-import { StyleSheet } from "react-native";
+import { AppState, StyleSheet } from "react-native";
 import { AuthProvider } from "./context/AuthContext";
 import AppNav from "./AppNav";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        console.log("App has come to the foreground!");
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log("AppState", appState.current);
+    });
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+  if (appStateVisible == "active") {
+    console.log("App is active from :", new Date());
+  }
+  // if (appStateVisible == "background") {
+  //   console.log("App is active till :", new Date());
+  // }
+
+  if (appStateVisible == "background") {
+    console.log("App is active till :", new Date());
+  }
+
+  // console.log("Current state is: " + appStateVisible);
+
   return (
     <AuthProvider>
       <AppNav />
