22c2295e5564e17ff82777f26efb065537d34c9c
kasunjalitha <kasunjalitha77@gmail.com>
2022-10-27 17:30:37 +0530

    db ip changed

--- Backend/helpers/mysql.init.js
+++ Backend/helpers/mysql.init.js
@@ -2,21 +2,21 @@ const mysql = require("mysql");
 const util = require("util");
 require("dotenv").config();
 
-// let connection = mysql.createConnection({
-//   host: "localhost",
-//   port: process.env.MYSQL_PORT,
-//   user: "root",
-//   password: "root",
-//   database: "trainlk",
-// });
-
 let connection = mysql.createConnection({
-  host: "sql6.freesqldatabase.com",
+  host: "localhost",
   port: process.env.MYSQL_PORT,
-  user: "sql6518660",
-  password: "YW7SiN4TrL",
-  database: "sql6518660",
+  user: "root",
+  password: "root",
+  database: "trainlk",
 });
+
+// let connection = mysql.createConnection({
+//   host: "sql6.freesqldatabase.com",
+//   port: process.env.MYSQL_PORT,
+//   user: "sql6518660",
+//   password: "YW7SiN4TrL",
+//   database: "sql6518660",
+// });
 
 // let connection = mysql.createConnection({
 //   host: "trainlk-db-server.mysql.database.azure.com",
@@ -26,7 +26,7 @@ let connection = mysql.createConnection({
 //   database: "trainlk",
 // });
 
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
     console.log("Error occurred ", err);
   } else {
--- MobileApp/utils.js
+++ MobileApp/utils.js
@@ -1,7 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const utils = { lanip: "http://10.22.164.157:8085/api/v1" };
 =======
 const utils = { lanip: "http://192.168.137.46:8080/api/v1" };
 >>>>>>> 03cf737 (navigation error fized)
+=======
+const utils = { lanip: "http://192.168.1.102:8080/api/v1" };
+>>>>>>> cd06bcc (db ip changed)
 
 export default utils;
